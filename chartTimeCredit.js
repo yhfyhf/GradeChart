d175daa05ee2cbefd0acb29f8b414322a7327a2f
yhfyhf <yhf406716870@gmail.com>
2014-04-10 12:02:28 +0800

    js files

---
+++ chartTimeCredit.js
@@ -0,0 +1,31 @@
+﻿function divideByTimeCredit(course) {  // divide courses into groups according to their time 
+	var up2011Sum = 0, down2011Sum = 0, up2012Sum = 0, down2012Sum =0, 
+		up2013Sum = 0, down2013Sum = 0, up2014Sum = 0, down2014Sum = 0;
+	/* get the SUM credits of every semester */ 
+	for(i=0; i<course.length; i++){
+		if(course[i].time == "2011 Spring")
+			{ up2011Sum += course[i].credit; }
+		if(course[i].time == "2011 Fall")
+			{ down2011Sum += course[i].credit;}
+		if(course[i].time == "2012 Spring")
+			{ up2012Sum += course[i].credit;}
+		if(course[i].time == "2012 Fall")
+			{ down2012Sum += course[i].credit;}
+		if(course[i].time == "2013 Spring")
+			{ up2013Sum += course[i].credit;}
+		if(course[i].time == "2013 Fall")
+			{ down2013Sum += course[i].credit;}
+		if(course[i].time == "2014 Spring")
+			{ up2014Sum += course[i].credit;}
+		if(course[i].time == "2014 Fall")
+			{ down2014Sum += course[i].credit;}
+	}
+	var dataPoints = "var timeCreditDataPoints = " + "[" + 
+						   "{ label: \"2011 Fall\", legendText: \"2011 Fall\", y: " + down2011Sum + " }," +
+						   "{ label: \"2012 Spring\", legendText: \"2012 Spring\", y: " + up2012Sum + " }," +
+						   "{ label: \"2012 Fall\", legendText: \"2012 Fall\", y: " + down2012Sum + " }," +
+						   "{ label: \"2013 Spring\", legendText: \"2013 Spring\", y: " + up2013Sum + " }," +
+						   "{ label: \"2013 Fall\", legendText: \"2013 Fall\", y: " + down2013Sum + " }" + "]";
+	return dataPoints;
+}
+
---
+++ getData.js
@@ -0,0 +1,124 @@
+﻿function getData(course) {
+	/* get TIME */
+	function getTime(){
+		var timeOption = document.getElementById("time");
+		var time = timeOption.options[timeOption.selectedIndex].value;
+		return time;
+	}
+
+	/* get courseName */
+	function getCourseName(){
+		var courseNameOption = document.getElementById("courseName");
+		var courseName = courseNameOption.value;
+		return courseName;
+	}
+
+	/* get courseType */
+	function getCourseType(){
+		var courseTypeOption = document.getElementsByName("identity");
+		for(var i=0; i<courseTypeOption.length; i++)
+			if (courseTypeOption[i].checked == true) 
+				break;
+		var courseType = courseTypeOption[i].value;
+		return courseType;
+	}
+
+	/* get credit */
+	function getCredit(){
+		var creditOption = document.getElementById("credit");
+		var credit = creditOption.value;
+		return credit;
+	}
+
+	function getScore(){
+		var scoreOption = document.getElementById("score");
+		var score = scoreOption.value;
+		return score;
+	}
+
+	function checkCredit(){
+		var credit = getCredit();
+		if( (credit>0) && (credit<=6) && (credit*2)==Math.floor(credit*2) )  //超出范围或不是以 ".0",".5" 结束
+			return credit;
+		else {
+			alert("请输入正确的学分！"); 
+			return 0;
+		}
+	}
+
+	function checkScore(){
+		var score = getScore();
+		if( (score>0) && (score<=100) )
+			return score;
+		else{ 
+			alert("请输入正确的分数！");
+			return 0;
+		}	
+	}
+
+	function jsonAppend(course, newCourse) {   // append the new json data to the current json data
+        course = JSON.stringify(course);
+        newCourse = "," + JSON.stringify(newCourse); 
+        course = course.replace("]", newCourse + "]");
+        course = course.replace("[,", "[");
+        return JSON.parse(course);
+    }
+
+	function addToCourseList(a, b, c, d, e, course){
+		var newCourse = { time: a, courseName: b, courseType: c, credit: d, score: e };
+		return jsonAppend(course, newCourse);
+	}
+
+
+
+	function check(course){
+		if ( checkCredit() && checkScore() ){
+			var time = getTime();
+			var courseName = getCourseName();	
+			var courseType = getCourseType();
+			var credit = checkCredit();
+			var score = checkScore();
+			addToClassList(time, courseName, courseType, credit, score);
+ 			return addToCourseList(time, courseName, courseType, credit, score, course); 
+		}
+		else 
+			return course;
+	}
+
+	function charToNum(){
+		course = check(course);
+		for(var i=0; i<course.length; i++){
+			course[i].credit = Number(course[i].credit);
+			course[i].score = Number(course[i].score); 
+		}
+		return course;
+	}
+
+	function addToClassList(time, courseName, courseType, credit, score) {
+		var courseList = document.getElementById("courseList");
+		var newCourse = document.createElement("tr");
+		newCourse.className = "list_row";
+		courseList.appendChild(newCourse); 
+		newCourse.innerHTML = "<td>" + time + "</td>"
+							+ "<td>" + courseName + "</td>"
+							+ "<td>" + courseType + "</td>"
+							+ "<td>" + credit + "</td>"
+							+ "<td>" + score + "</td>";
+	}
+
+	return charToNum();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
